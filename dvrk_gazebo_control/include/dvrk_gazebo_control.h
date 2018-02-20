1ccba79e6db121c8742fb2fea90abe23cf04ec65
ankur-agrawal <ankur.agrawal2409@gmail.com>
2018-02-19 19:26:47 -0500

    Everything checked, seems ok

--- dvrk_gazebo_control/include/dvrk_gazebo_control.h
+++ dvrk_gazebo_control/include/dvrk_gazebo_control.h
@@ -13,17 +13,40 @@
 
 class dvrk_gazebo_control{
 private:
-  std::vector<ros::Publisher> ecmPub;
+  std::vector<ros::Publisher> ecmPub, psm1Pub, psm2Pub, psm3Pub;
   ros::Subscriber link_states, image_sub;
   ros::Publisher plot_x, plot_y, plot_z;
   std::vector<ros::Publisher> cartPub;
 public:
   dvrk_gazebo_control(ros::NodeHandle n){
     ecmPub.resize(4);
-    ecmPub[0]=n.advertise<std_msgs::Float64>("/dvrk/ecm/yaw_joint/SetPosition",1000);
-    ecmPub[1]=n.advertise<std_msgs::Float64>("/dvrk/ecm/pitch_back_joint/SetPositionTarget",1000);
-    ecmPub[2]=n.advertise<std_msgs::Float64>("/dvrk/ecm/main_insertion_joint/SetPositionTarget",1000);
+    ecmPub[0]=n.advertise<std_msgs::Float64>("/dvrk/ecm/yaw_joint/SetPosition",10);
+    ecmPub[1]=n.advertise<std_msgs::Float64>("/dvrk/ecm/pitch_front_joint/SetPositionTarget",10);
+    ecmPub[2]=n.advertise<std_msgs::Float64>("/dvrk/ecm/main_insertion_joint/SetPosition",10);
     ecmPub[3]=n.advertise<std_msgs::Float64>("/dvrk/ecm/tool_joint/SetPosition",1000);
+
+
+    psm1Pub.resize(5);
+    psm1Pub[0]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_yaw_joint/SetPosition",10);
+    psm1Pub[1]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_pitch_joint_1/SetPositionTarget",10);
+    psm1Pub[2]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_insertion_joint/SetPosition",10);
+    psm1Pub[3]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_roll_joint/SetPosition",10);
+    psm1Pub[4]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/rev_joint/SetPosition",10);
+
+    psm2Pub.resize(5);
+    psm2Pub[0]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_yaw_joint/SetPosition",10);
+    psm2Pub[1]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_pitch_joint_1/SetPositionTarget",10);
+    psm2Pub[2]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_insertion_joint/SetPosition",10);
+    psm2Pub[3]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_roll_joint/SetPosition",10);
+    psm2Pub[4]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/rev_joint/SetPosition",10);
+
+    psm3Pub.resize(5);
+    psm3Pub[0]=n.advertise<std_msgs::Float64>("/dvrk/PSM3/outer_yaw_joint/SetPosition",1000);
+    psm3Pub[1]=n.advertise<std_msgs::Float64>("/dvrk/PSM3/outer_pitch_joint_1/SetPositionTarget",1000);
+    psm3Pub[2]=n.advertise<std_msgs::Float64>("/dvrk/PSM3/outer_insertion_joint/SetPosition",1000);
+    psm3Pub[3]=n.advertise<std_msgs::Float64>("/dvrk/PSM3/outer_roll_joint/SetPosition",1000);
+    psm3Pub[4]=n.advertise<std_msgs::Float64>("/dvrk/PSM3/rev_joint/SetPosition",1000);
+
     plot_x=n.advertise<std_msgs::Float64>("/plotx",1000);
     plot_y=n.advertise<std_msgs::Float64>("/ploty",1000);
     plot_z=n.advertise<std_msgs::Float64>("/plotz",1000);
@@ -33,7 +56,7 @@ public:
     {
       for (int j=0;j<5;j++)
       {
-        sprintf(topic, "/dvrk/SUJ/SUJ_PSM%d_J%d/SetPosition", i,j);
+        sprintf(topic, "/dvrk/SUJ/SUJ_psm1%d_J%d/SetPosition", i,j);
 
         cartPub[5*(i-1)+j] = n.advertise<std_msgs::Float64>(topic,1000);
       }
@@ -45,11 +68,14 @@ public:
       cartPub[15+j] = n.advertise<std_msgs::Float64>(topic,1000);
     }
     link_states=n.subscribe("/gazebo/link_states", 100, &dvrk_gazebo_control::getECMEndEffector, this);
-    image_sub = n.subscribe("/dvrk/ecm/camera/image_raw", 100, &dvrk_gazebo_control::showImage, this);
   }
 
   void showImage(const sensor_msgs::ImageConstPtr& img);
   void getECMEndEffector(const gazebo_msgs::LinkStatesPtr &msg);
   void PublishCartStates();
+  void PublishECMStates();
+  void PublishPSM1States();
+  void PublishPSM2States();
+  void PublishPSM3States();
 
 };
