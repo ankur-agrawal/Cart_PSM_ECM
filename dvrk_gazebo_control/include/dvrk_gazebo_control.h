2ebf5e7b6bb5a47f5fec1f70a21e34ecb4970d06
ankur-agrawal <ankur.agrawal2409@gmail.com>
2018-03-03 03:23:42 -0500

    IROS video stuff

--- dvrk_gazebo_control/include/dvrk_gazebo_control.h
+++ dvrk_gazebo_control/include/dvrk_gazebo_control.h
@@ -10,34 +10,52 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
-
+#include <sensor_msgs/JointState.h>
+#include <fstream>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
 class dvrk_gazebo_control{
 private:
   std::vector<ros::Publisher> ecmPub, psm1Pub, psm2Pub, psm3Pub;
-  ros::Subscriber link_states, image_sub;
+  ros::Subscriber link_states, joint_states, masterSub;
   ros::Publisher plot_x, plot_y, plot_z;
   std::vector<ros::Publisher> cartPub;
+  std::ofstream outdata;
+  std::ofstream outdata_rot;
+  Eigen::Vector4d lastMaster;
+  Eigen::Matrix4d ECM_base;
+  Eigen::Matrix4d ECM_base_to_tip;
+  Eigen::Matrix4d ECM_world_to_tip;
+  double ECM_joints[4];
+  double PSM1_joints[6];
+  double PSM2_joints[6];
+  Eigen::Matrix4d ECM_DH;
+  Eigen::Matrix4d psm1_base=Eigen::Matrix4d::Identity();
+  Eigen::Matrix4d psm1_tip=Eigen::Matrix4d::Identity();
+  Eigen::Matrix4d psm2_base=Eigen::Matrix4d::Identity();
+  Eigen::Matrix4d psm2_tip=Eigen::Matrix4d::Identity();
+  int init=1;
 public:
   dvrk_gazebo_control(ros::NodeHandle n){
     ecmPub.resize(4);
-    ecmPub[0]=n.advertise<std_msgs::Float64>("/dvrk/ecm/yaw_joint/SetPosition",10);
-    ecmPub[1]=n.advertise<std_msgs::Float64>("/dvrk/ecm/pitch_front_joint/SetPositionTarget",10);
-    ecmPub[2]=n.advertise<std_msgs::Float64>("/dvrk/ecm/main_insertion_joint/SetPosition",10);
+    ecmPub[0]=n.advertise<std_msgs::Float64>("/dvrk/ecm/yaw_joint/SetPositionTarget",1000);
+    ecmPub[1]=n.advertise<std_msgs::Float64>("/dvrk/ecm/pitch_front_joint/SetPositionTarget",1000);
+    ecmPub[2]=n.advertise<std_msgs::Float64>("/dvrk/ecm/main_insertion_joint/SetPositionTarget",1000);
     ecmPub[3]=n.advertise<std_msgs::Float64>("/dvrk/ecm/tool_joint/SetPosition",1000);
 
 
     psm1Pub.resize(5);
-    psm1Pub[0]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_yaw_joint/SetPosition",10);
-    psm1Pub[1]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_pitch_joint_1/SetPositionTarget",10);
-    psm1Pub[2]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_insertion_joint/SetPosition",10);
-    psm1Pub[3]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_roll_joint/SetPosition",10);
+    psm1Pub[0]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_yaw_joint/SetPositionTarget",1000);
+    psm1Pub[1]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_pitch_joint_1/SetPositionTarget",1000);
+    psm1Pub[2]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_insertion_joint/SetPositionTarget",1000);
+    psm1Pub[3]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/outer_roll_joint/SetPositionTarget",1000);
     psm1Pub[4]=n.advertise<std_msgs::Float64>("/dvrk/PSM1/rev_joint/SetPosition",10);
 
     psm2Pub.resize(5);
-    psm2Pub[0]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_yaw_joint/SetPosition",10);
-    psm2Pub[1]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_pitch_joint_1/SetPositionTarget",10);
-    psm2Pub[2]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_insertion_joint/SetPosition",10);
-    psm2Pub[3]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_roll_joint/SetPosition",10);
+    psm2Pub[0]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_yaw_joint/SetPositionTarget",1000);
+    psm2Pub[1]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_pitch_joint_1/SetPositionTarget",1000);
+    psm2Pub[2]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_insertion_joint/SetPositionTarget",1000);
+    psm2Pub[3]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/outer_roll_joint/SetPositionTarget",1000);
     psm2Pub[4]=n.advertise<std_msgs::Float64>("/dvrk/PSM2/rev_joint/SetPosition",10);
 
     psm3Pub.resize(5);
@@ -46,10 +64,14 @@ public:
     psm3Pub[2]=n.advertise<std_msgs::Float64>("/dvrk/PSM3/outer_insertion_joint/SetPosition",1000);
     psm3Pub[3]=n.advertise<std_msgs::Float64>("/dvrk/PSM3/outer_roll_joint/SetPosition",1000);
     psm3Pub[4]=n.advertise<std_msgs::Float64>("/dvrk/PSM3/rev_joint/SetPosition",1000);
-
+    //
     plot_x=n.advertise<std_msgs::Float64>("/plotx",1000);
     plot_y=n.advertise<std_msgs::Float64>("/ploty",1000);
     plot_z=n.advertise<std_msgs::Float64>("/plotz",1000);
+
+    outdata.open("ecm_yaw_step.csv");
+    outdata_rot.open("ecm_yaw_angle.csv");
+
     cartPub.resize(19);
     char topic[100];
     for (int i=1;i<4;i++)
@@ -68,14 +90,38 @@ public:
       cartPub[15+j] = n.advertise<std_msgs::Float64>(topic,1000);
     }
     link_states=n.subscribe("/gazebo/link_states", 100, &dvrk_gazebo_control::getECMEndEffector, this);
-  }
+    joint_states=n.subscribe("/dvrk/joint/states", 100, &dvrk_gazebo_control::readJointStates, this);
+    masterSub=n.subscribe("/dvrk/MTM/LeftPose", 100, &dvrk_gazebo_control::readMaster, this);
+    setECM_base();
+    setECM_DH();
+    ECM_FK();
 
+    const geometry_msgs::Pose msg;
+    readMaster(msg);
+    // Eigen::Quaterniond q;
+    // std::cout << q.toRotationMatrix() << '\n';
+    // std::cout <<Eigen::Quaterniond(1,0,0,0) << '\n';
+    // Eigen::MatrixXd pos(3,1);
+    // pos(0,0)=ECM_base(0,3);
+    // pos(1,0)=ECM_base(1,3);
+    // pos(2,0)=ECM_base(2,3);
+    // std::cout << pos << '\n';
+    // PSM_IK(pos, PSM1_joints);
+    // std::cout << PSM1_joints[0] << '\t' << PSM1_joints[1] << '\t'<< PSM1_joints[2] << '\n';
+  }
+  void setECM_DH();
+  void setECM_base();
+  Eigen::Matrix4d compute_mod_dh_matrix(double alpha, double a, double theta, double d);
+  void ECM_FK();
+  void PSM_IK(Eigen::MatrixXd pos, double (&PSM_joints)[6]);
   void showImage(const sensor_msgs::ImageConstPtr& img);
   void getECMEndEffector(const gazebo_msgs::LinkStatesPtr &msg);
+  void readJointStates(const sensor_msgs::JointStatePtr &msg);
+  void readMaster(const geometry_msgs::Pose &msg);
   void PublishCartStates();
   void PublishECMStates();
-  void PublishPSM1States();
-  void PublishPSM2States();
+  void PublishPSM1States(double PSM_joints[6]);
+  void PublishPSM2States(double PSM_joints[6]);
   void PublishPSM3States();
 
 };
--- dvrk_gazebo_control/src/dvrk_gazebo_control.cpp
+++ dvrk_gazebo_control/src/dvrk_gazebo_control.cpp
@@ -1,48 +1,319 @@
 #include "dvrk_gazebo_control.h"
+static double pitch_angle=0;
+static double yaw_angle=0;
+static double direction;
+static double iter;
+static bool step_pitch=1;
+static bool step_yaw=0;
+
+Eigen::Matrix4d dvrk_gazebo_control::compute_mod_dh_matrix(double alpha, double a, double theta, double d)
+{
+  Eigen::Matrix4d DH_matrix;
+  DH_matrix(0,0)=cos(theta);
+  DH_matrix(0,1)=-sin(theta);
+  DH_matrix(0,2)=0;
+  DH_matrix(0,3)=a;
+  DH_matrix(1,0)=sin(theta)*cos(alpha);
+  DH_matrix(1,1)=cos(theta)*cos(alpha);
+  DH_matrix(1,2)=-sin(alpha);
+  DH_matrix(1,3)=-d*sin(alpha);
+  DH_matrix(2,0)=sin(theta)*sin(alpha);
+  DH_matrix(2,1)=cos(theta)*sin(alpha);
+  DH_matrix(2,2)=cos(alpha);
+  DH_matrix(2,3)=d*cos(alpha);
+  DH_matrix(3,0)=0;
+  DH_matrix(3,1)=0;
+  DH_matrix(3,2)=0;
+  DH_matrix(3,3)=1;
+  return DH_matrix;
+}
+
+void dvrk_gazebo_control::setECM_DH()
+{
+
+  ECM_DH(0,0)=1.5708;
+  ECM_DH(0,1)=0;
+  ECM_DH(0,2)=ECM_joints[0]+1.5708;
+  ECM_DH(0,3)=0;
+  ECM_DH(1,0)=-1.5708;
+  ECM_DH(1,1)=0;
+  ECM_DH(1,2)=ECM_joints[1]-1.5708;
+  ECM_DH(1,3)=0;
+  ECM_DH(2,0)=1.5708;
+  ECM_DH(2,1)=0;
+  ECM_DH(2,2)=0;
+  ECM_DH(2,3)=ECM_joints[2]-0.3822;
+  ECM_DH(3,0)=0;
+  ECM_DH(3,1)=0;
+  ECM_DH(3,2)=ECM_joints[3];
+  ECM_DH(3,3)=0.3829;
+
+}
+
+void dvrk_gazebo_control::ECM_FK()
+{
+  ECM_base_to_tip << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1;
+  for (int i=0; i<4;i++)
+  {
+    ECM_base_to_tip=ECM_base_to_tip * compute_mod_dh_matrix(ECM_DH(i,0),ECM_DH(i,1),ECM_DH(i,2),ECM_DH(i,3));
+  }
+  ECM_world_to_tip=ECM_base*ECM_base_to_tip;
+}
+
+void dvrk_gazebo_control::setECM_base()
+{
+  ECM_base(0,0)=0;
+  ECM_base(0,1)=1;
+  ECM_base(0,2)=0;
+  ECM_base(0,3)=0.6126;
+  ECM_base(1,0)=-1;
+  ECM_base(1,1)=0;
+  ECM_base(1,2)=0;
+  ECM_base(1,3)=0;
+  ECM_base(2,0)=0;
+  ECM_base(2,1)=0;
+  ECM_base(2,2)=1;
+  ECM_base(2,3)=1.6016;
+  ECM_base(3,0)=0;
+  ECM_base(3,1)=0;
+  ECM_base(3,2)=0;
+  ECM_base(3,3)=1;
+}
+void dvrk_gazebo_control::PSM_IK(Eigen::MatrixXd pos, double (&PSM_joints)[6])
+{
+  // double PSM_joints[6];
+  PSM_joints[0]=atan2(pos(0,0),-pos(1,0));
+  PSM_joints[1]=atan2(-(pos(2,0)+0.369),sqrt(pow(pos(0,0),2)+pow(pos(1,0),2)));
+  PSM_joints[2]=sqrt(pow(pos(0,0),2)+pow(pos(1,0),2)+pow((pos(2,0)+0.369),2));
+  PSM_joints[3]=0;
+  PSM_joints[4]=0;
+  PSM_joints[5]=0;
+}
+
+void dvrk_gazebo_control::readMaster(const geometry_msgs::Pose &Master)
+{
+  if (init==1)
+  {
+    lastMaster(0,0)=Master.position.x;
+    lastMaster(1,0)=Master.position.y;
+    lastMaster(2,0)=Master.position.z;
+    lastMaster(3,0)=1;
+    init=0;
+    return;
+  }
+
+  Eigen::MatrixXd diff(4,1);
+  Eigen::MatrixXd psm1_tip_pos(4,1), PSM1_tip(4,1);
+  Eigen::Matrix4d PSM1_tip_in_ecm;
+  // PSM1_tip_in_ecm=ECM_world_to_tip.inverse()*psm1_tip;
+  psm1_tip_pos(0,3)=psm1_tip(0,0);
+  psm1_tip_pos(1,3)=psm1_tip(1,0);
+  psm1_tip_pos(2,3)=psm1_tip(2,0);
+  diff(0,0)=Master.position.x-lastMaster(0,0);
+  diff(1,0)=Master.position.y-lastMaster(1,0);
+  diff(2,0)=Master.position.z-lastMaster(2,0);
+  diff(3,0)=1;
+  // PSM1_tip_in_ecm_pos=PSM1_tip_in_ecm_pos+diff;
+  PSM1_tip=psm1_tip_pos+ECM_world_to_tip*diff;
+  PSM_IK(PSM1_tip, PSM1_joints);
+  PublishPSM1States(PSM1_joints);
+  std::cout << PSM1_tip << '\n';
+}
+
+void dvrk_gazebo_control::readJointStates(const sensor_msgs::JointStatePtr &msg)
+{
+  std_msgs::Float64 msg2;
+  for (int i=0;i<msg->name.size();i++)
+  {
+    if (step_pitch)
+    {
+      if (!msg->name[i].compare("dvrk/ecm/pitch_front_joint"))
+      {
+        msg2.data=msg->position[i];
+        plot_x.publish(msg2);
+        outdata << msg->header.stamp<< '\t' << msg->position[i] << '\n';
+        outdata_rot << msg->header.stamp<< '\t' << pitch_angle << '\n';
+      }
+    }
+    if (step_yaw)
+    {
+      if (!msg->name[i].compare("dvrk/ecm/yaw_joint"))
+      {
+        msg2.data=msg->position[i];
+        plot_x.publish(msg2);
+        outdata << msg->header.stamp<< '\t' << msg->position[i] << '\n';
+        outdata_rot << msg->header.stamp<< '\t' << yaw_angle << '\n';
+      }
+    }
+  }
+}
 
 void dvrk_gazebo_control::getECMEndEffector(const gazebo_msgs::LinkStatesPtr &msg)
 {
-  gazebo_msgs::LinkState ecm_roll;
+  gazebo_msgs::LinkState ecm_rcm;
+
+
+  // if( !outdata ) { // file couldn't be opened
+  //     std::cerr << "Error: file could not be opened" << std::endl;
+  //     exit(1);
+  //  }
   for (int i=0;i<msg->pose.size();i++)
   {
-    if (!msg->name[i].compare("dvrk::ecm::camera_link"))
+    if (!msg->name[i].compare("dvrk::PSM1::outer_insertion_link"))
     // if (!msg->name[i].compare("dvrk::PSM1::one_tool_wrist_link"))
     {
-      ecm_roll.pose = msg->pose[i];
+      ecm_rcm.pose = msg->pose[i];
+    }
+    if (!msg->name[i].compare("dvrk::PSM1::remote_center_link"))
+    // if (!msg->name[i].compare("dvrk::PSM1::one_tool_wrist_link"))
+    {
+      psm1_base(0,3) = msg->pose[i].position.x;
+      psm1_base(1,3) = msg->pose[i].position.y;
+      psm1_base(2,3) = msg->pose[i].position.z;
+      Eigen::Quaterniond q(msg->pose[i].orientation.w,msg->pose[i].orientation.x,msg->pose[i].orientation.y,msg->pose[i].orientation.z);
+      // q.normalize();
+      Eigen::Matrix3d R = q.toRotationMatrix();
+      psm1_base(0,0)=R(0,0);
+      psm1_base(0,1)=R(0,1);
+      psm1_base(0,2)=R(0,2);
+      psm1_base(1,0)=R(1,0);
+      psm1_base(1,1)=R(1,1);
+      psm1_base(1,2)=R(1,2);
+      psm1_base(2,0)=R(2,0);
+      psm1_base(2,1)=R(2,1);
+      psm1_base(2,2)=R(2,2);
+    }
+    if (!msg->name[i].compare("dvrk::PSM1::tool_main_link"))
+    {
+      psm1_tip(0,3) = msg->pose[i].position.x;
+      psm1_tip(1,3) = msg->pose[i].position.y;
+      psm1_tip(2,3) = msg->pose[i].position.z;
+      Eigen::Quaterniond q(msg->pose[i].orientation.w,msg->pose[i].orientation.x,msg->pose[i].orientation.y,msg->pose[i].orientation.z);
+      // q.normalize();
+      Eigen::Matrix3d R = q.toRotationMatrix();
+      psm1_tip(0,0)=R(0,0);
+      psm1_tip(0,1)=R(0,1);
+      psm1_tip(0,2)=R(0,2);
+      psm1_tip(1,0)=R(1,0);
+      psm1_tip(1,1)=R(1,1);
+      psm1_tip(1,2)=R(1,2);
+      psm1_tip(2,0)=R(2,0);
+      psm1_tip(2,1)=R(2,1);
+      psm1_tip(2,2)=R(2,2);
+    }
+    if (!msg->name[i].compare("dvrk::PSM2::remote_center_link"))
+    // if (!msg->name[i].compare("dvrk::PSM1::one_tool_wrist_link"))
+    {
+      psm2_base(0,3) = msg->pose[i].position.x;
+      psm2_base(1,3) = msg->pose[i].position.y;
+      psm2_base(2,3) = msg->pose[i].position.z;
+      Eigen::Quaterniond q(msg->pose[i].orientation.w,msg->pose[i].orientation.x,msg->pose[i].orientation.y,msg->pose[i].orientation.z);
+      // q.normalize();
+      Eigen::Matrix3d R = q.toRotationMatrix();
+      psm2_base(0,0)=R(0,0);
+      psm2_base(0,1)=R(0,1);
+      psm2_base(0,2)=R(0,2);
+      psm2_base(1,0)=R(1,0);
+      psm2_base(1,1)=R(1,1);
+      psm2_base(1,2)=R(1,2);
+      psm2_base(2,0)=R(2,0);
+      psm2_base(2,1)=R(2,1);
+      psm2_base(2,2)=R(2,2);
+    }
+    if (!msg->name[i].compare("dvrk::PSM2::tool_main_link"))
+    {
+      psm2_tip(0,3) = msg->pose[i].position.x;
+      psm2_tip(1,3) = msg->pose[i].position.y;
+      psm2_tip(2,3) = msg->pose[i].position.z;
+      Eigen::Quaterniond q(msg->pose[i].orientation.w,msg->pose[i].orientation.x,msg->pose[i].orientation.y,msg->pose[i].orientation.z);
+      // q.normalize();
+      Eigen::Matrix3d R = q.toRotationMatrix();
+      psm2_tip(0,0)=R(0,0);
+      psm2_tip(0,1)=R(0,1);
+      psm2_tip(0,2)=R(0,2);
+      psm2_tip(1,0)=R(1,0);
+      psm2_tip(1,1)=R(1,1);
+      psm2_tip(1,2)=R(1,2);
+      psm2_tip(2,0)=R(2,0);
+      psm2_tip(2,1)=R(2,1);
+      psm2_tip(2,2)=R(2,2);
     }
   }
-  std::cout << ecm_roll.pose.position.x << '\t' << ecm_roll.pose.position.y << '\t' << ecm_roll.pose.position.z<< '\n';
-  // std_msgs::Float64 msg2;
-  // msg2.data=ecm_roll.pose.position.x;
+  // Eigen::Matrix4d psm_base_to_tip = psm_base.inverse()*psm_tip;
+  // Eigen::MatrixXd pos(3,1);
+  // pos << psm_tip(0,3)-psm_base(0,3), psm_tip(1,3)-psm_base(1,3), psm_tip(2,3)-psm_base(2,3);
+  // PSM_IK(pos, PSM1_joints);
+  // std::cout << pos(0,0) << '\t' << pos(1,0) << '\t' << pos(2,0)<< '\n';
+  // std::cout << PSM1_joints[0] <<'\t' << PSM1_joints[1] <<'\t'<< PSM1_joints[2] <<'\t'<< '\n';
+
+  // outdata << ecm_rcm.pose.position.x << '\t' << ecm_rcm.pose.position.y << '\t' << ecm_rcm.pose.position.z<< '\n';
+  // outdata_rot << ecm_rcm.pose.orientation.x << '\t' << ecm_rcm.pose.orientation.y << '\t' << ecm_rcm.pose.orientation.z << '\t' << ecm_rcm.pose.orientation.w << '\n';
+  // outdata.close();
+  std_msgs::Float64 msg2;
+  // msg2.data=ecm_rcm.pose.position.x;
   // plot_x.publish(msg2);
-  // msg2.data=ecm_roll.pose.position.y;
+  // msg2.data=ecm_rcm.pose.position.y;
   // plot_y.publish(msg2);
-  // msg2.data=ecm_roll.pose.position.z;
+  // msg2.data=ecm_rcm.pose.position.z;
   // plot_z.publish(msg2);
-  // msg2.data=0;
-  // ecmPub[0].publish(msg2);
-  // ecmPub[3].publish(msg2);
+  msg2.data=0;
+  ecmPub[3].publish(msg2);
+  psm1Pub[3].publish(msg2);
   //
-  // static double pitch_angle;
-  // static double direction;
-  // msg2.data=pitch_angle;
-  // if (pitch_angle>0.7)
+  if (iter > 5000)
+  {
+    if (step_pitch)
+      pitch_angle=0.5;
+    if (step_yaw)
+      yaw_angle=1;
+  }
+  else
+  {
+    // if (step_pitch)
+      pitch_angle=0;
+    // if (step_yaw)
+      yaw_angle=0;
+  }
+
+  iter=iter+1;
+
+
+  // if (pitch_angle>0.5)
+  // {
   //   direction=1;
-  // if (pitch_angle<-0.2)
+  //   yaw_angle=yaw_angle+0.1;
+  // }
+  // if (yaw_angle > 1)
+  // {
+  //   yaw_angle=1;
+  // }
+  // if (pitch_angle<-0.5)
   //   direction=0;
   // if (direction==0)
   //   pitch_angle=pitch_angle+0.001;
   // else
   //   pitch_angle=pitch_angle-0.001;
+
+  // std::cout << pitch_angle << '\n';
+  // msg2.data=pitch_angle;
   // ecmPub[1].publish(msg2);
+  // psm1Pub[1].publish(msg2);
+  //
+  // // std::cout << yaw_angle << '\n';
+  // // yaw_angle=0;
+  // msg2.data=yaw_angle;
+  // ecmPub[0].publish(msg2);
+  // psm1Pub[0].publish(msg2);
+  //
   // msg2.data=0;
   // ecmPub[2].publish(msg2);
+  // psm1Pub[2].publish(msg2);
   // msg2.data=0;
 
   // PublishCartStates();
-  PublishECMStates();
-  PublishPSM1States();
-  PublishPSM2States();
+  // PublishECMStates();
+  // PublishPSM1States();
+  // PublishPSM2States();
   // PublishPSM3States();
 
 }
@@ -63,24 +334,24 @@ void dvrk_gazebo_control::PublishECMStates()
 
 }
 
-void dvrk_gazebo_control::PublishPSM1States()
+void dvrk_gazebo_control::PublishPSM1States(double PSM_joints[6])
 {
   std::vector<std_msgs::Float64> msg;
   msg.resize(5);
-  msg[0].data=0.5;
-  msg[1].data=0;
-  msg[2].data=0;
-  msg[3].data=0;
-  msg[4].data=0;
+  msg[0].data=PSM_joints[0];
+  msg[1].data=PSM_joints[1];
+  msg[2].data=PSM_joints[2];
+  msg[3].data=PSM_joints[3];
+  // msg[4].data=PSM_joints[4];
 
   psm1Pub[0].publish(msg[0]);
   psm1Pub[1].publish(msg[1]);
   psm1Pub[2].publish(msg[2]);
   psm1Pub[3].publish(msg[3]);
-  psm1Pub[4].publish(msg[4]);
+  // psm1Pub[4].publish(msg[4]);
 
 }
-void dvrk_gazebo_control::PublishPSM2States()
+void dvrk_gazebo_control::PublishPSM2States(double PSM_joints[6])
 {
   std::vector<std_msgs::Float64> msg;
   msg.resize(5);
